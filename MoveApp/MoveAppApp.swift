cf157e7a837473074fb8c7d63fbbbcebc51004fd
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-03-14 11:09:11 +0200

    setting up the app

---
+++ MoveApp/MoveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveAppApp.swift
+//  MoveApp
+//
+//  Created by asmaa gamal  on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
